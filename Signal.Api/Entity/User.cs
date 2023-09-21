27ad19d057c2552ddfb16e258b96485b51b1d80e
Your Name <you@example.com>
2023-09-21 12:37:25 +0500

    Add user entity

---
+++ Signal.Api/Entity/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Signal.Api.Entity
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber {get; set;}
+        public Guid GrupId { get; set; }
+
+    }
+}
